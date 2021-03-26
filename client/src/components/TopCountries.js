ee21635dc77c5b8c448e7ca1f09101c66a5faf4a
UrLover <jonah_louis@outlook.com>
2021-03-26 14:08:40 -0400

    latest changes

--- client/src/components/TopCountries.js
+++ client/src/components/TopCountries.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Select from 'antd/lib/select'
-import Input from 'antd/lib/input'
 import { Column } from '@antv/g2plot'
 
 class TopCountries extends React.Component {
@@ -17,48 +16,61 @@ class TopCountries extends React.Component {
     sortByRecoveries = () => {
         this.props.COUNTRIES.sort((a, b) => parseInt(b.TotalRecovered) - parseInt(a.TotalRecovered));
     }
-    handleChange = (event) => {
-        const targetValue = event.target.value;
-    }
-    data = [
-        {"Country":this.props.COUNTRIES[0].Country, "Cases":this.props.COUNTRIES[0].Cases,
-        "Deaths":this.props.COUNTRIES[0].TotalDeaths, "Recoveries":this.props.COUNTRIES[0].TotalRecovered},
-        {"Country":this.props.COUNTRIES[1].Country, "Cases":this.props.COUNTRIES[1].Cases,
-        "Deaths":this.props.COUNTRIES[1].TotalDeaths, "Recoveries":this.props.COUNTRIES[1].TotalRecovered},
-        {"Country":this.props.COUNTRIES[2].Country, "Cases":this.props.COUNTRIES[2].Cases,
-        "Deaths":this.props.COUNTRIES[2].TotalDeaths, "Recoveries":this.props.COUNTRIES[2].TotalRecovered},
-        {"Country":this.props.COUNTRIES[3].Country, "Cases":this.props.COUNTRIES[3].Cases,
-        "Deaths":this.props.COUNTRIES[3].TotalDeaths, "Recoveries":this.props.COUNTRIES[3].TotalRecovered},
-    ]
 
     componentDidMount() {
-        const data = this.data;
-        const chartDom = this.chartNodeRef.current;
+        // Sort cases by default
         this.sortByCases();
+
+        // Assign data to constant
+        const data = [
+            {"Country":this.props.COUNTRIES[0].Country, "Cases":this.props.COUNTRIES[0].Cases,
+            "Deaths":this.props.COUNTRIES[0].TotalDeaths, "Recoveries":this.props.COUNTRIES[0].TotalRecovered},
+            {"Country":this.props.COUNTRIES[1].Country, "Cases":this.props.COUNTRIES[1].Cases,
+            "Deaths":this.props.COUNTRIES[1].TotalDeaths, "Recoveries":this.props.COUNTRIES[1].TotalRecovered},
+            {"Country":this.props.COUNTRIES[2].Country, "Cases":this.props.COUNTRIES[2].Cases,
+            "Deaths":this.props.COUNTRIES[2].TotalDeaths, "Recoveries":this.props.COUNTRIES[2].TotalRecovered},
+            {"Country":this.props.COUNTRIES[3].Country, "Cases":this.props.COUNTRIES[3].Cases,
+            "Deaths":this.props.COUNTRIES[3].TotalDeaths, "Recoveries":this.props.COUNTRIES[3].TotalRecovered},
+        ];
+        const chartDom = this.chartNodeRef.current;
         const column = new Column(chartDom, {
             data,
             xField: 'Country',
             yField: 'Cases',
             seriesField: 'x',
-            legend: true,
+            legend: false,
         })
 
         column.render();
         this.chartRef.current = column;
     }
 
+    handleChange = (v) => {
+        const column = this.chartRef.current;
+        if (column) {
+            column.update({ animation: v !== '' ? true : { appear: { animation: v } } });
+        }
+    }
+
     render () {
         return (
             <>
-            <div>
-                <span className="sort_type">Select</span>
-                <Select aria-label="select" defaultValue="Sort by most cases" size="small">
-                    {['Sort by most cases', 'Sort by most deaths', 'Sort by most recoveries'].map((opt) => {
-                        return <Select.Option value={opt}>{opt}</Select.Option>
-                    })}    
-                </Select>
-            </div>
-            <div ref={this.chartNodeRef}/>
+                <div>
+                    <span className="sort_type">Select</span>
+                    <Select 
+                    aria-label="select" 
+                    defaultValue="Sort by most cases" 
+                    size="small" 
+                    onChange={this.handleChange}
+                    >
+                        {['Sort by most cases', 
+                        'Sort by most deaths', 
+                        'Sort by most recoveries'].map((opt) => {
+                            return <Select.Option value={opt}>{opt}</Select.Option>
+                        })}    
+                    </Select>
+                </div>
+                <div ref={this.chartNodeRef} />
             </>
         )
     }
