1585961dadce044cd66e9cbe549d6da27b1bcc7b
UrLover <jonah_louis@outlook.com>
2021-03-27 11:14:55 -0400

    Change from G2plot to AVA

--- client/src/components/TopCountries.js
+++ client/src/components/TopCountries.js
@@ -1,78 +1,117 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState, useRef, useEffect } from 'react'
 import Select from 'antd/lib/select'
+import Row from 'antd/lib/row'
+import Col from 'antd/lib/col'
 import { Column } from '@antv/g2plot'
+import { dataToDataProps, dataPropsToAdvices, 
+    adviceToLibConfig, g2plotRender } from '@antv/chart-advisor';
 
-class TopCountries extends React.Component {
-    chartNodeRef = React.createRef();
-    chartRef = React.createRef(); 
+const { Option } = Select;
 
-    sortByCases = () => {
-        this.props.COUNTRIES.sort((a, b) => parseInt(b.Cases) - parseInt(a.Cases));
+const TopCountries = ({COUNTRIES}) => {
+    const [data, setData] = useState("Sort by most cases");
+    const [current, setCurrent] = useState();
+    const [advices, setAdvices] = useState([]);
+    const canvas = useRef();
+
+    const sortByCases = () => {
+        COUNTRIES.sort((a, b) => parseInt(b.Cases) - parseInt(a.Cases));
     }
-    sortByDeaths = () => {
-        this.props.COUNTRIES.sort((a, b) => parseInt(b.TotalDeaths) - parseInt(a.TotalDeaths));
+    const sortByDeaths = () => {
+        COUNTRIES.sort((a, b) => parseInt(b.TotalDeaths) - parseInt(a.TotalDeaths));
     }
-    sortByRecoveries = () => {
-        this.props.COUNTRIES.sort((a, b) => parseInt(b.TotalRecovered) - parseInt(a.TotalRecovered));
+    const sortByRecoveries = () => {
+        COUNTRIES.sort((a, b) => parseInt(b.TotalRecovered) - parseInt(a.TotalRecovered));
     }
+    useEffect(() => {
+        const chartData = getData(data); 
 
-    componentDidMount() {
-        // Sort cases by default
-        this.sortByCases();
+        // step 1: data -> data props
+        const dataProps = dataToDataProps(chartData);
+        console.log('dataProps: ', dataProps);
 
-        // Assign data to constant
-        const data = [
-            {"Country":this.props.COUNTRIES[0].Country, "Cases":this.props.COUNTRIES[0].Cases,
-            "Deaths":this.props.COUNTRIES[0].TotalDeaths, "Recoveries":this.props.COUNTRIES[0].TotalRecovered},
-            {"Country":this.props.COUNTRIES[1].Country, "Cases":this.props.COUNTRIES[1].Cases,
-            "Deaths":this.props.COUNTRIES[1].TotalDeaths, "Recoveries":this.props.COUNTRIES[1].TotalRecovered},
-            {"Country":this.props.COUNTRIES[2].Country, "Cases":this.props.COUNTRIES[2].Cases,
-            "Deaths":this.props.COUNTRIES[2].TotalDeaths, "Recoveries":this.props.COUNTRIES[2].TotalRecovered},
-            {"Country":this.props.COUNTRIES[3].Country, "Cases":this.props.COUNTRIES[3].Cases,
-            "Deaths":this.props.COUNTRIES[3].TotalDeaths, "Recoveries":this.props.COUNTRIES[3].TotalRecovered},
-        ];
-        const chartDom = this.chartNodeRef.current;
-        const column = new Column(chartDom, {
-            data,
-            xField: 'Country',
-            yField: 'Cases',
-            seriesField: 'x',
-            legend: false,
-        })
+        // step 2: data props -> Advices( chart type + vega lite specs)
+        const advices = dataPropsToAdvices(dataProps);
+        console.log('advices: ', advices);
 
-        column.render();
-        this.chartRef.current = column;
-    }
+        setAdvices(advices);
+        setCurrent(advices[0].type);
+    }, [data]);
 
-    handleChange = (v) => {
-        const column = this.chartRef.current;
-        if (column) {
-            column.update({ animation: v !== '' ? true : { appear: { animation: v } } });
+    useEffect(() => {
+        const chartData = getData(data);
+        if (current && canvas.current) {
+          // step 3: vega lite spec -> lib config
+          const typeSpec = advices.find((s) => s.type === current);
+          console.log('typeSpec: ', typeSpec);
+    
+          // step 4: lib config -> chart render & return instance
+          const libConfig = adviceToLibConfig(typeSpec);
+          console.log('libConfig: ', libConfig);
+    
+          canvas.current.innerHTML = null;
+          // step 5: render
+          g2plotRender(canvas.current, chartData, libConfig);
         }
-    }
+      }, [data, current]);
 
-    render () {
-        return (
-            <>
-                <div>
-                    <span className="sort_type">Select</span>
-                    <Select 
-                    aria-label="select" 
-                    defaultValue="Sort by most cases" 
-                    size="small" 
-                    onChange={this.handleChange}
-                    >
-                        {['Sort by most cases', 
-                        'Sort by most deaths', 
-                        'Sort by most recoveries'].map((opt) => {
-                            return <Select.Option value={opt}>{opt}</Select.Option>
-                        })}    
-                    </Select>
-                </div>
-                <div ref={this.chartNodeRef} />
-            </>
-        )
+    return (
+        <>
+            <Select
+            value={data}
+            onChange={(e) => {
+                setData(e);}}
+            style={{ width: 200 }}
+            >
+                <Option value="Sort by most cases">Sort by most cases</Option>
+                <Option value="Sort by most deaths">Sort by most deaths</Option>
+                <Option value="Sort by most recoveries">Sort by most recoveries</Option>
+            </Select>
+            <Col span={18}>
+                <div ref={canvas} style={{ minHeight: 300 }} />
+            </Col> 
+        </>
+    )
+
+    function getData(sortType) {
+        if (sortType === "Sort by most cases") { sortByCases(); }
+        if (sortType === "Sort by most deaths") { sortByDeaths(); }
+        if (sortType === "Sort by most recoveries") { sortByRecoveries(); }
+
+        const countries_cases = [
+            {"Country"      :COUNTRIES[0].Country,      
+            "Cases"         :COUNTRIES[0].Cases},
+            {"Country"      :COUNTRIES[1].Country,      
+            "Cases"         :COUNTRIES[1].Cases},
+            {"Country"      :COUNTRIES[2].Country,      
+            "Cases"         :COUNTRIES[2].Cases},
+            {"Country"      :COUNTRIES[3].Country,      
+            "Cases"         :COUNTRIES[3].Cases},
+        ];
+        const countries_deaths = [
+            {"Country"      :COUNTRIES[0].Country,      
+            "Deaths"        :COUNTRIES[0].TotalDeaths},  
+            {"Country"      :COUNTRIES[1].Country,      
+            "Deaths"        :COUNTRIES[1].TotalDeaths},  
+            {"Country"      :COUNTRIES[2].Country,      
+            "Deaths"        :COUNTRIES[2].TotalDeaths},  
+            {"Country"      :COUNTRIES[3].Country,      
+            "Deaths"        :COUNTRIES[3].TotalDeaths},  
+        ];
+        const countries_recoveries = [
+            {"Country"      :COUNTRIES[0].Country,      
+            "Recoveries"    :COUNTRIES[0].TotalRecovered},
+            {"Country"      :COUNTRIES[1].Country,      
+            "Recoveries"    :COUNTRIES[1].TotalRecovered},
+            {"Country"      :COUNTRIES[2].Country,      
+            "Recoveries"    :COUNTRIES[2].TotalRecovered},
+            {"Country"      :COUNTRIES[3].Country,      
+            "Recoveries"    :COUNTRIES[3].TotalRecovered},
+        ];
+
+        if (sortType === "Sort by most cases") { return countries_cases; }
+        if (sortType === "Sort by most deaths") { return countries_deaths }
+        if (sortType === "Sort by most recoveries") { return countries_recoveries; }
     }
 }
 
