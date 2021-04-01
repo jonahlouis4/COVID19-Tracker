7200ab3b2b5d87f4644983434344a9bc7fb55e5c
UrLover <jonah_louis@outlook.com>
2021-04-01 01:16:39 -0400

    New build

---
+++ public/build/precache-manifest.caf0759c686aa78013d4fe509ade7ab8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2015d10154975d3549515f505903b1df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61a0995fb238b9cae6ca",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "7f6eccef1b36000fcb89",
+    "url": "/static/css/main.135abb57.chunk.css"
+  },
+  {
+    "revision": "61a0995fb238b9cae6ca",
+    "url": "/static/js/2.baabf9bc.chunk.js"
+  },
+  {
+    "revision": "078c72d1a3858c7f89f1943a48b93efa",
+    "url": "/static/js/2.baabf9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6eccef1b36000fcb89",
+    "url": "/static/js/main.2274b95b.chunk.js"
+  },
+  {
+    "revision": "2a3496e0349049e73fa1",
+    "url": "/static/js/runtime-main.9aef5679.js"
+  },
+  {
+    "revision": "19cbb8638e622a90d5284c052b11fc25",
+    "url": "/static/media/background-header-1.19cbb863.svg"
+  }
+]);
