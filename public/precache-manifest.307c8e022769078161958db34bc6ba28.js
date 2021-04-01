4936601cc1a7a2cbbb3d328dafffb9acf797466a
UrLover <jonah_louis@outlook.com>
2021-03-31 21:53:57 -0400

    Added heroku

---
+++ public/precache-manifest.307c8e022769078161958db34bc6ba28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624ac3fcd8b1244aa0c0c99c79d57afc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61a0995fb238b9cae6ca",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "8554cbd28698f128941e",
+    "url": "/static/css/main.acea6cfc.chunk.css"
+  },
+  {
+    "revision": "61a0995fb238b9cae6ca",
+    "url": "/static/js/2.baabf9bc.chunk.js"
+  },
+  {
+    "revision": "078c72d1a3858c7f89f1943a48b93efa",
+    "url": "/static/js/2.baabf9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8554cbd28698f128941e",
+    "url": "/static/js/main.74c8d519.chunk.js"
+  },
+  {
+    "revision": "2a3496e0349049e73fa1",
+    "url": "/static/js/runtime-main.9aef5679.js"
+  },
+  {
+    "revision": "19cbb8638e622a90d5284c052b11fc25",
+    "url": "/static/media/background-header-1.19cbb863.svg"
+  }
+]);
